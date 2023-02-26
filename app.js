36eacf6d87ce0f635c5aefe5bd40a5b5ee6d4c89
Jaqueline <jaqueline1@ice.ufjf.br>
2023-02-26 15:08:56 -0300

    Configurando handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // Cria servidor
 const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 const db = require('./db/connection');
 const bodyParcer = require('body-parser');
 
@@ -10,15 +12,21 @@ app.listen(PORT);
 
 app.use(bodyParcer.urlencoded({extended: false}));
 
+// Handle bars
+// Diretorio das views, onde ficara os tamplates
+app.set('views', path.join(__dirname, 'views'));
+// Aquivo principal de layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// View engine, framework/biblioteca que sera utilizada para renderizar as views
+app.set('view engine', 'handlebars');
+
 // Data base connection
 db.authenticate()
-
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Tela Principal');
 });
-
 
 // Jobs Routes 
 app.use('/jobs', require('./routes/jobs'));
