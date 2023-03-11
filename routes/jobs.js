a414fb5010f1599fa0d94ec537bd30425ffea6ee
Jaqueline <jaqueline1@ice.ufjf.br>
2023-03-10 21:24:15 -0300

    Criando view de detalhes do job
    Criando view onde serão exibidos os dados e detalhes da vaga

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+// Detalhe da vaga
+router.get('/view/:id', (req, res) => 
+    
+    Job.findOne({ where: {id: req.params.id} })
+    .then(job => {
+        res.render('layouts/view', {job});
+    })
+    .catch(err => console.log(err))
+);
+
+// Form da rota de envio
 router.get('/add', (req, res) => {
     res.render('layouts/add');
 });
@@ -11,7 +22,7 @@ router.post('/add', (req, res) => {
 
     let {title, description, salary, company, email, new_job} = req.body;
     
-    // Insert new job
+    // Insere novo job
     Job.create({
         title,
         description,
